4134eba4b607629141ddd3afac41dcc3e7758905
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-11 00:19:29 -0400

    setting up redux

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,12 +5,19 @@ import App from "./App";
 
 /* Redux */
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 import rootReducer from "./reducers";
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
